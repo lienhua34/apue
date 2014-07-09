98df4d7aa0d9768c33fc388c158dc31d81dd37e1
lienhua34 <lienhua34@163.com>
2014-07-09 22:36:13 +0800

    add alarmdemo.c sigclddemo.c

---
+++ ch10/examples/alarmdemo.c
@@ -0,0 +1,33 @@
+#include "../../common/apue.h"
+#include <pwd.h>
+
+static void
+my_alarm(int signo) 
+{
+  struct passwd *rootptr;
+  
+  printf("in signal handler\n");
+  if ((rootptr = getpwnam("root")) == NULL) {
+    err_sys("getpwnam(root) error");
+  }
+  alarm(1);
+}
+
+int
+main(void)
+{
+  struct passwd *ptr;
+  
+  signal(SIGALRM, my_alarm);
+  alarm(1);
+  for (; ; ) {
+    if ((ptr = getpwnam("lienhua34")) == NULL) {
+      err_sys("getpwnam error");
+    }
+    //    alarm(1);
+    if (strcmp(ptr->pw_name, "lienhua34") != 0) {
+      printf("return value corrupted!, pw_name = %s\n", ptr->pw_name);
+    }
+    //    printf("pw_name = %s\n", ptr->pw_name);
+  }
+}
---
+++ ch10/examples/sigclddemo.c
@@ -0,0 +1,36 @@
+#include "../../common/apue.h"
+#include <sys/wait.h>
+
+static void sig_cld(int);
+
+int
+main()
+{
+  pid_t pid;
+  
+  if (signal(SIGCLD, sig_cld) == SIG_ERR) {
+    perror("signal error");
+  }
+  if ((pid = fork()) < 0) {
+    perror("fork error");
+  } else if (pid == 0) {
+    sleep(2);
+    _exit(0);
+  }
+}
+
+static void
+sig_cld(int signo)
+{
+  pid_t pid;
+  int status;
+
+  printf("SIGCLD received.\n");
+  if (signal(SIGCLD, sig_cld) == SIG_ERR) {
+    perror("sigal error");
+  } 
+  if ((pid == wait(&status)) < 0) {
+    perror("wait error");
+  }
+  printf("pid = %d\n", pid);
+}
