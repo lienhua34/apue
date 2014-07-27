48da01c3cbf949243541c825ca1ad8db2def2871
lienhua34 <lienhua34@163.com>
2014-07-27 11:25:04 +0800

    learn nonblock io

---
+++ ch14/examples/set_fl.c
@@ -0,0 +1,18 @@
+#include "../../common/apue.h"
+#include <fcntl.h>
+
+void
+set_fl(int fd, int falgs)
+{
+  int val;
+
+  if ((val = fcntl(fd, F_GETFL, 0)) < 0) {
+    err_sys("fcntl F_GETFL error");
+  }
+
+  val |= falgs;
+
+  if (fcntl(fd, F_SETFL, val) < 0) {
+    err_sys("fcntl F_SETFL error");
+  }
+}
