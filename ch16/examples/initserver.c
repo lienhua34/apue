909a4ce7aa84ef6655f67d3971d013f193419a8f
lienhua34 <lienhua34@163.com>
2014-08-12 23:17:47 +0800

    learn socket options

--- ch16/examples/initserver.c
+++ ch16/examples/initserver.c
@@ -7,9 +7,14 @@ initserver(int type, const struct sockaddr *addr, socklen_t alen, int qlen)
 {
   int fd;
   int err = 0;
+  int reuse = 1;
 
   if ((fd = socket(addr->sa_family, type, 0)) < 0) {
     return(-1);
+  }
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0) {
+    err = errno;
+    goto errout;
   }
   if (bind(fd, addr, alen) < 0) {
     err = errno;
